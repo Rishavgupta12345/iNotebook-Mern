daca33e2d92a51fb0d219637051f35061dc7a1ba
Rishavgupta12345 <rishavgupta933@gmail.com>
2024-12-09 19:20:17 +0530

    request response mapping

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,23 @@
-const connectToMongo = require('./db');
-const express = require('express')
-connectToMongo(); 
+const connectToMongo = require("./db");
+const express = require("express");
+connectToMongo();
 
-
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000;
 
 //Old code
 // app.get('/', (req, res) => {
 //   res.send('Hello World chal bsdk!')
 // })
 
-//New version 
+//New version
+
+app.use(express.json());
 
 //Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,9 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const {
+  body,
+  validatorResult,
+  validationResult,
+} = require("express-validator");
 
-router.get('/',(req,res)=>{
-  
-    res.json([]) 
-})
+// create a user using : POST "/api/auth/createuser". Doesn't require auth
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //if there are errors, return Bad Request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router
+    try {
+      //Check whether the user with this email existis already
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
+  }
+);
+
+module.exports = router;
