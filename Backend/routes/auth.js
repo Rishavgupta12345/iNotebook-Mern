09991c22a0df52a4c096bf37cfb5871cb50a43e8
Rishavgupta12345 <rishavgupta933@gmail.com>
2024-12-11 15:40:01 +0530

    salt added login endpoint done

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {
   body,
   validatorResult,
   validationResult,
 } = require("express-validator");
+
+const JWT_SECRET = "Rishavisagoodboy";
 
 // create a user using : POST "/api/auth/createuser". Doesn't require auth
 router.post(
@@ -32,18 +36,75 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password,salt)
+
+      //create a new user
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
 
-      res.json(user);
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const jwtData = jwt.sign(data, JWT_SECRET);
+
+      res.json({jwtData});
+
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error occured");
+      res.status(500).send("Internal server error");
     }
   }
 );
+
+// authenticate a user using : POST "/api/auth/login". Doesn't require auth 
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password can not be blank").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+    try{
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({error: "Incorrect credientials"});
+      }
+
+      const passwordCompare = await bcrypt.compare(password,user.password);
+      if(!passwordCompare){
+        return res.status(400).json({error: "Incorrect credientials"});
+      }
+
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+
+      const jwtData = jwt.sign(data, JWT_SECRET);
+      res.json({jwtData});
+
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+
+  }
+)
+  
+
 
 module.exports = router;
