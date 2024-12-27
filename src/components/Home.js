2c162bbd1983f4b75b7111b1c439f04b7c78e47b
Rishavgupta12345 <rishavgupta933@gmail.com>
2024-12-27 15:04:15 +0530

    basic edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,74 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/NoteContext";
 
 const Home = () => {
+  const context = useContext(noteContext);
+  const { notes, addNote, deleteNote } = context;
+
   return (
     <div>
       <div className="container my-3">
-      <h1>Add a Note</h1>
-      <form>
-        <div className="my-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
+        <h1>Add a Note</h1>
+        <form>
+          <div className="my-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
           </div>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-      <h1>You Notes</h1>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+        <h1>You Notes</h1>
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <div className="card my-3">
+                <div className="card-body">
+                  <h5 className="card-title">{note.title}</h5>
+                  <p className="card-text">{note.description}</p>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      deleteNote(note._id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
